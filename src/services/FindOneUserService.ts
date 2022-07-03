3e4b7b91ee86ddf95c898b904d91708dec8dd995
elvesbd <elvesbd@yahoo.com>
2022-07-03 17:51:33 -0300

    chore: create validation with user not exists

--- src/services/FindOneUserService.ts
+++ src/services/FindOneUserService.ts
@@ -2,8 +2,13 @@ import { User } from '../models/User';
 import UserRepository from '../repositories/UserRepository';
 
 class FindOneUserService {
-  async execute(username: string): Promise<User | null> {
-    return UserRepository.findOneUser(username);
+  async execute(username: string): Promise<User | Error | null> {
+    const userExists = await UserRepository.findOneUser(username);
+
+    if (userExists) {
+      return new Error(`User ${username} already exists`);
+    }
+    return userExists;
   }
 }
 
